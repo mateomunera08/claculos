21f62e1b7b4a984e22f95a4ca686961340f41896
mateomunera <mateomunera2004@gmail.com>
2022-03-03 17:27:26 -0500

    calculadora

---
+++ calculos.js
@@ -0,0 +1,17 @@
+alert("heee")
+let numero=10;
+let numero2=20;
+//operadores aritmericos comentario
+let suma=numero+numero2;
+let resta=numero-numero2;
+let multiplicacion=numero*numero2;
+let division=numero/numero2;
+
+console.log("la suma es:"+suma);
+console.log("la resta es"+resta);
+console.log("la multiplicacion es"+multiplicacion);
+console.log("la divicion es"+division);
+//recibiendo datos por teclado
+let edad=Number(prompt("digita tu edad"))
+let resultado=edad+10;
+console.log(resultado);
